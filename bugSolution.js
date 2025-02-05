6cca5cc46d2c05a0b9e07c520373e038b9e07605
BugHunter-X <askzenai@gmail.com>
2025-02-05 21:38:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,19 @@
+```javascript
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log('Count:', count); 
+    return () => {
+      console.log('Component unmounting');
+    };
+  }, [count]); // Only re-run the effect if 'count' changes
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}
+```
